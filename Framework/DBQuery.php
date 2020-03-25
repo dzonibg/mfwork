8bd5ece5486519cb5695d0363d5718612aef201b
Nikola <nidza.bg@gmail.com>
2020-03-25 22:00:55 +0100

    Added DB Query and Model master class.

---
+++ Framework/DBQuery.php
@@ -0,0 +1,25 @@
+<?php
+
+class Model {
+
+    public function db()
+    {
+        $host = $GLOBALS['db_hostname'];
+        $db = $GLOBALS['db_name'];
+        $user = $GLOBALS['db_username'];
+        $pass = $GLOBALS['db_password'];
+
+        $dsn = "mysql:host=$host;dbname=$db;";
+        $options = [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+            PDO::ATTR_EMULATE_PREPARES   => false,
+        ];
+        try {
+            $pdo = new PDO($dsn, $user, $pass, $options);
+            return $pdo;
+        } catch (\PDOException $e) {
+            throw new \PDOException($e->getMessage(), (int)$e->getCode());
+        }
+    }
+}
--- config.php
+++ config.php
@@ -1,3 +1,10 @@
 <?php
+/*APP:*/
 
 $GLOBALS['debug'] = true;
+
+/*Database:*/
+$GLOBALS['db_hostname'] = 'localhost';
+$GLOBALS['db_name'] = 'mfwork';
+$GLOBALS['db_username'] = 'root';
+$GLOBALS['db_password'] = '';
