54c9a2f5e3d211d5521c0462a4d14986c2cbd822
Nikola <nidza.bg@gmail.com>
2020-04-06 06:09:13 +0200

    DBQuery got the create & store function.

--- Framework/DBQuery.php
+++ Framework/DBQuery.php
@@ -43,4 +43,34 @@ class Model {
     public function insert($values) {
         $this->db()->query("INSERT INTO " . $this->tableName . " VALUES (" . $values . ");");
     }
+
+    public function create($object) {
+        $array = (array)$object;
+        foreach ($array as $key => $value) {
+            if (($key != "tableName") && ($key != 'values') && ($key != 'keys') && ($key != 'id')) {
+                $this->addKey($key);
+                $this->addValue($value);
+            }
+        }
+        $this->keys = mb_substr($this->keys, 0, -2);
+        $this->values = mb_substr($this->values, 0, -2);
+        print $this->keys;
+        print $this->values;
+        $this->store();
+    }
+
+    public function addKey($key) {
+        $this->keys = $this->keys . $key . ", ";
+    }
+
+    public function addValue($value) {
+        $this->values = $this->values . "'" . $value . "', ";
+    }
+
+    public function store() {
+        $query = 'INSERT INTO ' . $this->tableName . ' (' . $this->keys . ')' . ' VALUES (' . $this->values . ');';
+        echo $query;
+        $this->db()->query($query);
+
+    }
 }
--- Models/Users.php
+++ Models/Users.php
@@ -17,34 +17,4 @@ class Users extends Model {
 
     }
 
-    public function create(Users $user) {
-        $array = (array)$user;
-        foreach ($array as $key => $value) {
-            if (($key != "tableName") && ($key != 'values') && ($key != 'keys') && ($key != 'id')) {
-                $this->addKey($key);
-                $this->addValue($value);
-            }
-        }
-        $this->keys = mb_substr($this->keys, 0, -2);
-        $this->values = mb_substr($this->values, 0, -2);
-        print $this->keys;
-        print $this->values;
-        $this->store();
-    }
-
-    public function addKey($key) {
-        $this->keys = $this->keys . $key . ", ";
-    }
-
-    public function addValue($value) {
-        $this->values = $this->values . "'" . $value . "', ";
-    }
-
-    public function store() {
-        $query = 'INSERT INTO ' . $this->tableName . ' (' . $this->keys . ')' . ' VALUES (' . $this->values . ');';
-        echo $query;
-        $this->db()->query($query);
-
-    }
-
 }
